b0fa51011a0b4a64667918538ea2403f44dc1b73
Reza <Reza@gmal.com>
2024-08-06 14:37:09 +0700

    Sudah menambahkan button OK

--- app/src/main/java/com/example/aplikasi1/MainActivity.java
+++ app/src/main/java/com/example/aplikasi1/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.aplikasi1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Button btnOk;
         setContentView(R.layout.activity_main);
+        btnOk = (Button) findViewById(R.id.ok);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnOk.setBackgroundColor(Color.RED);
+                Toast.makeText(MainActivity.this, "Tombol Menjadi Merah", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
