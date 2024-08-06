4432ddb15d0f0c49d3c1cbf2bed31f798abe91f9
Reza <Reza@gmal.com>
2024-08-06 15:59:37 +0700

    Sudah membuat item menu 1,2,3 dan page 1,2,3

--- app/src/main/java/com/example/aplikasi1/MainActivity.java
+++ app/src/main/java/com/example/aplikasi1/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.aplikasi1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Button btnOk;
         setContentView(R.layout.activity_main);
         btnOk = (Button) findViewById(R.id.ok);
+
         btnOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,5 +30,25 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Tombol Menjadi Merah", Toast.LENGTH_SHORT).show();
             }
         });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.optionmenu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.menu1){
+            startActivity(new Intent(this, Menu1.class));
+        } else  if (item.getItemId()==R.id.menu2){
+            startActivity(new Intent(this, Menu2.class));
+        } else  if (item.getItemId()==R.id.menu3){
+            startActivity(new Intent(this, Menu3.class));
+        }
+        return true;
     }
 }
